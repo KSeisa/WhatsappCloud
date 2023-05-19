1f8729b4873aa03a6f422df671eae58228ac4458
Andre Croukamp <andre.croukamp@entelect.co.za>
2023-05-20 00:53:54 +0200

    aa

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const { v4: uuidv4 } = require('uuid');
 require('dotenv').config();
 const app = express().use(bodyParser.json());
 
@@ -11,7 +12,8 @@ const { SESSION_SECRET, VERIFY_TOKEN, PORT} = process.env;
 app.use(session({
   secret: SESSION_SECRET,
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
+  genid: () => uuidv4(), 
 }));
 
 app.listen(PORT, () => console.log(`webhook is listening on port ${PORT}`));
