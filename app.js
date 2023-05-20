ff503188e78350b5e57c28de4d11d020346e9e46
Andre Croukamp <andre.croukamp@entelect.co.za>
2023-05-20 10:36:57 +0200

    aa

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const  MongoClient  = require('mongodb');
+const  MongoClient  = require('mongodb').MongoClient;
 
 const app = express().use(bodyParser.json());
 
