9513448d7367f067675574832590cbe69babbe17
Andre Croukamp <andre.croukamp@entelect.co.za>
2023-05-20 19:26:01 +0200

    ss

--- Controllers/mongodbController.js
+++ Controllers/mongodbController.js
@@ -1,71 +1,75 @@
-async function updateDocumentById(number, updateFields, client) {
-    try {
-      const database = client.db('Entelect');
-      const collection = database.collection('HealthCheck');
-  
-      const result = await collection.updateOne(
-        { _id: number },
-        { $set: updateFields }
-      );
-  
-      if (result.matchedCount === 1) {
-        console.log('Document updated successfully');
-      } else {
-        console.log('Document not found');
-      }
-    } catch (err) {
-      console.error('Error connecting to MongoDB:', err);
-    }
+const { MongoClient } = require('mongodb');
+
+const uri = "mongodb+srv://dbUser:dbUserPassword@cluster0.lh84toi.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+}
+
+async function closeDatabaseConnection() {
+  try {
+    await client.close();
+    console.log('Disconnected from MongoDB');
+  } catch (err) {
+    console.error('Error disconnecting from MongoDB:', err);
+  }
 }
 
 async function testSessionExist(number) {
-    try {
-      const database = client.db('Entelect');
-      const collection = database.collection('HealthCheck');
-  
-      const existingDoc = await collection.findOne({ _id: number });
-  
-      if (existingDoc) {
-          return existingDoc;
-        } else {
-          const newDoc = { 
-              _id: number,
-              backToMainMenu: true,
-              testSessionID: false,
-              userInputSessionID: '',
-              testSessionIDMenu: false,
-          };
-          const result = await collection.insertOne(newDoc);
-          console.log('New document added for: ', number);
-          return newDoc;
-        }
-    } catch (err) {
-      console.error('Error connecting to MongoDB:', err);
-      return null;
+  try {
+    const database = client.db('Entelect');
+    const collection = database.collection('HealthCheck');
+
+    const existingDoc = await collection.findOne({ _id: number });
+
+    if (existingDoc) {
+      return existingDoc;
+    } else {
+      const newDoc = { 
+        _id: number,
+        backToMainMenu: true,
+        testSessionID: false,
+        userInputSessionID: '',
+        testSessionIDMenu: false,
+      };
+      const result = await collection.insertOne(newDoc);
+      console.log('New document added for: ', number);
+      return newDoc;
     }
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    return null;
+  }
 }
 
-async function endSessionDelete(number, client) {
-    try {
-      const database = client.db('Entelect');
-      const collection = database.collection('HealthCheck');
-  
-      const result = await collection.deleteOne({ _id: number });
-  
-      if (result.deletedCount === 1) {
-        console.log('Document deleted successfully');
-        return true;
-      } else {
-        console.log('Document not found');
-      }
-    } catch (err) {
-      console.error('Error connecting to MongoDB:', err);
+async function endSessionDelete(number) {
+  try {
+    const database = client.db('Entelect');
+    const collection = database.collection('HealthCheck');
+
+    const result = await collection.deleteOne({ _id: number });
+
+    if (result.deletedCount === 1) {
+      console.log('Document deleted successfully');
+      return true;
+    } else {
+      console.log('Document not found');
     }
-    return false;
-} 
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+  return false;
+}
 
 module.exports = {
-    updateDocumentById,
-    testSessionExist,
-    endSessionDelete,
-}
+  connectToDatabase,
+  closeDatabaseConnection,
+  testSessionExist,
+  endSessionDelete,
+};
