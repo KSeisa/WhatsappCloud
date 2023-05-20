b5670bb380db593abfcf65469b2eefbc35e720c5
Andre Croukamp <andre.croukamp@entelect.co.za>
2023-05-20 19:27:01 +0200

    ss

--- Controllers/incommingMessageController.js
+++ Controllers/incommingMessageController.js
@@ -3,12 +3,28 @@ const { welcomeMessageStep, resetSessionVariables, endSessionMessage,
         viewSessionNotes, viewSessionSummary, viewTrends } = require('./sessionController');
 const { sendBasicMessage } = require('./whatsappMessageController');
 
-const {
-  connectToDatabase,
-  closeDatabaseConnection,
-  testSessionExist,
-  endSessionDelete
-} = require('./mongodbController');
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://dbUser:dbUserPassword@cluster0.lh84toi.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri);
+
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+}
+
+async function closeDatabaseConnection() {
+  try {
+    await client.close();
+    console.log('Disconnected from MongoDB');
+  } catch (err) {
+    console.error('Error disconnecting from MongoDB:', err);
+  }
+}
 
 async function incomingMessageHandler(req, res) {
   if (testIncomingMessage(req)) {
@@ -76,6 +92,52 @@ function testIncomingMessage(req) {
   } catch (error) {
     return false;
   }
+}
+
+async function testSessionExist(number) {
+  try {
+    const database = client.db('Entelect');
+    const collection = database.collection('HealthCheck');
+
+    const existingDoc = await collection.findOne({ _id: number });
+
+    if (existingDoc) {
+        return existingDoc;
+      } else {
+        const newDoc = { 
+            _id: number,
+            backToMainMenu: true,
+            testSessionID: false,
+            userInputSessionID: '',
+            testSessionIDMenu: false,
+        };
+        const result = await collection.insertOne(newDoc);
+        console.log('New document added for: ', number);
+        return newDoc;
+      }
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+    return null;
+  }
+}
+
+async function endSessionDelete(number, client) {
+  try {
+    const database = client.db('Entelect');
+    const collection = database.collection('HealthCheck');
+
+    const result = await collection.deleteOne({ _id: number });
+
+    if (result.deletedCount === 1) {
+      console.log('Document deleted successfully');
+      return true;
+    } else {
+      console.log('Document not found');
+    }
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err);
+  }
+  return false;
 }
 
 connectToDatabase();
