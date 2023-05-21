f919f7ce2c3a7ff3c32017146d5695a9c549114a
Andre Croukamp <andre.croukamp@entelect.co.za>
2023-05-21 21:40:32 +0200

    ss

--- Controllers/whatsappMessageController.js
+++ Controllers/whatsappMessageController.js
@@ -1,25 +1,25 @@
-// const { createBot } = require('whatsapp-cloud-api');
+const { createBot } = require('whatsapp-cloud-api');
 
-// const { AUTH_TOKEN, PHONE_NUMBER_ID } = process.env;
+const { AUTH_TOKEN, PHONE_NUMBER_ID } = process.env;
 
-// const bot = createBot(PHONE_NUMBER_ID, AUTH_TOKEN);
+const bot = createBot(PHONE_NUMBER_ID, AUTH_TOKEN);
 
-// async function sendBasicMessage(to, body) {
-//   try {
-//     await bot.sendText(to, body);
-//   } catch (error) {
-//     console.error('Error sending message:', error.message);
-//   }
-// }
+async function sendBasicMessage(to, body) {
+  try {
+    await bot.sendText(to, body);
+  } catch (error) {
+    console.error('Error sending message:', error.message);
+  }
+}
 
-// async function sendBasicMediaMessage(to, imageUrl) {
-//   try {
-//     const result = await bot.sendImage(to, imageUrl);
-//     console.log('Media message sent successfully');
-//   } catch (error) {
-//     console.error('Error sending media message:', error.message);
-//   }
-// }
+async function sendBasicMediaMessage(to, imageUrl) {
+  try {
+    const result = await bot.sendImage(to, imageUrl);
+    console.log('Media message sent successfully');
+  } catch (error) {
+    console.error('Error sending media message:', error.message);
+  }
+}
 ////////////////////
 // const axios = require('axios');
 // const { AUTH_TOKEN, PHONE_NUMBER_ID } = process.env;
@@ -46,42 +46,6 @@
 //     console.error('Error sending message:', error.message);
 //   }
 // }
-
-const axios = require('axios');
-
-const url = 'https://graph.facebook.com/v16.0/108570732243051/messages';
-const accessToken = 'EAAjatt8FjXwBACo302dPR7yDX34N1lyzWJAoDg4makbQXq4nP2ZAPCtouBUofZCLsn4dxhZBRNoZBFZCJG5YuCDSCGlphppFjiTcPNETkJzF6lvQNBVXbaAmKt0MiRB7F4WGx5zZAL0fw75T0JyXgSlnpq39BA3HHebHedjeEktj4S7Y266okc6t553NZBruZBpu1kUFDJnFbylGzI3ExdSHOJlvHssY9ZCoZD';
-
-async function sendBasicMessage(to, body) {
-  const data = {
-    messaging_product: 'whatsapp',
-    to: '27760411047',
-    type: 'text',
-    text: 'Hello, world!'
-  };
-
-  try {
-    const response = await axios.post(url, data, {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-        'Content-Type': 'application/json'
-      }
-    });
-
-    console.log(response.data);
-  } catch (error) {
-    console.error('Error sending message:', error.message);
-  }
-}
-
-async function sendBasicMediaMessage(to, imageUrl) {
-  try {
-    const result = await bot.sendImage(to, imageUrl);
-    console.log('Media message sent successfully');
-  } catch (error) {
-    console.error('Error sending media message:', error.message);
-  }
-}
 
 module.exports = {
   sendBasicMessage,
