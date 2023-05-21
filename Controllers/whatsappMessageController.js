38dba5b18e2d89f147f86024664a63b14a73dbbe
Andre Croukamp <andre.croukamp@entelect.co.za>
2023-05-21 21:27:49 +0200

    ss

--- Controllers/whatsappMessageController.js
+++ Controllers/whatsappMessageController.js
@@ -6,10 +6,7 @@ const bot = createBot(PHONE_NUMBER_ID, AUTH_TOKEN);
 
 async function sendBasicMessage(to, body) {
   try {
-    console.log(AUTH_TOKEN);
-    console.log(PHONE_NUMBER_ID);
-    console.log(bot);
-    const result = await bot.sendText(to, body);
+    await bot.sendText(to, body);
   } catch (error) {
     console.error('Error sending message:', error.message);
   }
